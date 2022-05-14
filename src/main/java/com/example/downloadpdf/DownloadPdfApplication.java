7a5125fddd32aced2de36e63d1071528938a045d
mederesenali <mederesenali@gmail.com>
2022-05-14 13:02:25 +0600

    refactor

--- src/main/java/com/example/downloadpdf/DownloadPdfApplication.java
+++ src/main/java/com/example/downloadpdf/DownloadPdfApplication.java
@@ -1,6 +1,5 @@
 package com.example.downloadpdf;
 
-import com.lowagie.text.DocumentException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -16,31 +14,17 @@ import java.util.Date;
 @SpringBootApplication
 @Controller
 public class DownloadPdfApplication {
-
     @Autowired
     private PdfExporterService pdfExporterService;
 
-    @GetMapping("/")
-    public void exportToPDF(HttpServletResponse response) throws DocumentException, IOException {
-//        response.setContentType("application/pdf");
-//        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
-//        String currentDateTime = dateFormatter.format(new Date());
-//        String headerKey = "Content-Disposition";
-//        String headerValue = "attachment; filename=users_" + currentDateTime + ".pdf";
-//        response.setHeader(headerKey, headerValue);
-//        pdfExporterService.export(response);
-
-    }
-    @GetMapping("/pdf/generate")
-    public void generatePDF(HttpServletResponse response) throws IOException {
+    @GetMapping("api/pdf/generate")
+    public void generatePDF(HttpServletResponse response){
         response.setContentType("application/pdf");
         DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd:hh:mm:ss");
         String currentDateTime = dateFormatter.format(new Date());
-
         String headerKey = "Content-Disposition";
         String headerValue = "attachment; filename=pdf_" + currentDateTime + ".pdf";
         response.setHeader(headerKey, headerValue);
-
         pdfExporterService.export(response);
     }
 
--- src/main/java/com/example/downloadpdf/PdfExporterService.java
+++ src/main/java/com/example/downloadpdf/PdfExporterService.java
@@ -1,10 +1,7 @@
 package com.example.downloadpdf;
 
-import java.awt.Color;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
-
 import javax.servlet.http.HttpServletResponse;
 
 import com.lowagie.text.*;
@@ -15,86 +12,22 @@ import org.springframework.stereotype.Service;
 public class PdfExporterService {
     public static final String FONT = "static/Arial Cyr.ttf";
 
-    private void writeTableHeader(PdfPTable table) {
-        PdfPCell cell = new PdfPCell();
-       // cell.setBackgroundColor(Color.BLUE);
-        cell.setPadding(5);
-
-
-
-        Font font = FontFactory.getFont(FontFactory.HELVETICA,"utf-16");
-        ((com.lowagie.text.Font) font).setColor(Color.WHITE);
-
-        cell.setPhrase(new Phrase("abc", font));
-
-        table.addCell(cell);
-
-        cell.setPhrase(new Phrase("аыфваываы", font));
-        table.addCell(cell);
-
-        cell.setPhrase(new Phrase("аывфаыаывф", font));
-        table.addCell(cell);
-
-        cell.setPhrase(new Phrase("аываывалжывалы", font));
-        table.addCell(cell);
-
-        cell.setPhrase(new Phrase("ваожвыаыжфалыфжвж", font));
-        table.addCell(cell);
-    }
-
-
-//
-//    public void export(HttpServletResponse response) throws DocumentException, IOException {
-//        Document document = new Document(PageSize.A4);
-//        PdfWriter.getInstance(document, response.getOutputStream());
-//
-//        document.open();
-//        com.lowagie.text.Font font = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-//        font.setSize(18);
-//       // font.setColor(Color.BLUE);
-//
-//        Paragraph p = new Paragraph("List of Users", font);
-//        p.setAlignment(Paragraph.ALIGN_CENTER);
-//
-//        document.add(p);
-//
-//        PdfPTable table = new PdfPTable(5);
-//        table.setWidthPercentage(100f);
-//        table.setWidths(new float[] {1.5f, 3.5f, 3.0f, 3.0f, 1.5f});
-//        table.setSpacingBefore(10);
-//
-//        writeTableHeader(table);
-//
-//        document.add(table);
-//
-//        document.close();
-//
-//    }
-    public void export(HttpServletResponse response) throws IOException {
-        Document document = new Document(PageSize.A4);
-        PdfWriter.getInstance(document, response.getOutputStream());
-
-        BaseFont russianFont=BaseFont.createFont(FONT, BaseFont.IDENTITY_H,BaseFont.EMBEDDED);//RECOGNIZED
-
-
-
-
-        String germanString = "This is a тайтл.";
-        byte[] germanBytes = germanString.getBytes();
-        String value = new String(germanBytes, StandardCharsets.UTF_8);
-
-        document.open();
-       Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD); //  ????
-
-        Font font=new Font(russianFont);
-       // Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);//.
-        fontTitle.setSize(18);
-
-
-        Paragraph paragraph = new Paragraph(value, font);
-        paragraph.setAlignment(Paragraph.ALIGN_CENTER);
-
-        document.add(paragraph);
-        document.close();
+    public void export(HttpServletResponse response) {
+        try (Document document = new Document(PageSize.A4)){
+            PdfWriter.getInstance(document, response.getOutputStream());
+            BaseFont russianFont = BaseFont.createFont(FONT, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            String germanString = "This is a тайтл.";
+            byte[] germanBytes = germanString.getBytes();
+            String value = new String(germanBytes, StandardCharsets.UTF_8);
+            document.open();
+            Font fontTitle = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+            Font font = new Font(russianFont);
+            fontTitle.setSize(18);
+            Paragraph paragraph = new Paragraph(value, font);
+            paragraph.setAlignment(Element.ALIGN_CENTER);
+            document.add(paragraph);
+        }catch (IOException ioException){
+            ioException.printStackTrace();
+        }
     }
 }
